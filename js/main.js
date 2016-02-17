2d54fd899ada2bf4065cea9873dbc629460ee5f7
ropez-94 <dleonard@mail.bradley.edu>
2016-02-17 05:53:52 -0600

    Solid base updated to true material design, foundation is here, now content needs added.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 /**
  * Created by dakotaleonard on 2/14/16.
  */
+var onHeaderClick = function(eventObject)
+{
+    var thisElement = $(this);
+    if(thisElement.hasClass("z-depth-5"))
+    {
+        thisElement.removeClass("z-depth-5");
+    }
+    else
+    {
+        thisElement.addClass("z-depth-5");
+    }
+};
+
 $(document).ready(function()
 {
     $(".button-collapse").sideNav();
-    $('.blur').scrollSpy();
+   // $('.header').scrollSpy();
+
+    $(".header").click(onHeaderClick);
 });
